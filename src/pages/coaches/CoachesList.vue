46f4b5f2638da22748703b4c687af977e594ae8a
Omid <o.shabani@hotmail.com>
2023-06-13 16:14:38 +0330

    implement coaches page

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,23 +6,43 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coache-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :areas="coach.areas"
+        :rate="coach.hourlyRate"
+      ></coache-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 <script>
+import coacheItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: {
+    coacheItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coachesStore/coaches'];
     },
     hasCoaches() {
-
       return this.$store.getters['coachesStore/hasCoaches'];
     },
   },
 };
 </script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
