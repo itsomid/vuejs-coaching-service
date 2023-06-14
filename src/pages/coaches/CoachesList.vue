a0826ec3ba4ccd3b81f14ed22a0e3d5035c08950
Omid <o.shabani@hotmail.com>
2023-06-14 16:02:16 +0330

    implement base i for badge,button,card

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,21 +1,24 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coache-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :areas="coach.areas"
-        :rate="coach.hourlyRate"
-      ></coache-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register" >Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coache-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :areas="coach.areas"
+          :rate="coach.hourlyRate"
+        ></coache-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 <script>
@@ -26,6 +29,7 @@ export default {
   },
   computed: {
     filteredCoaches() {
+      console.log(this.$store.getters['coachesStore/coaches']);
       return this.$store.getters['coachesStore/coaches'];
     },
     hasCoaches() {
