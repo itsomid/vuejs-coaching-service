3a1dcdfbf0e18ca1ea8d03aba396335af270f1e0
Omid <o.shabani@hotmail.com>
2023-07-09 16:59:15 +0330

    implement spinner loading

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,9 +6,14 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button v-if="!isCoach"  link to="/register">Register as Coach</base-button>
+        <base-button v-if="!isCoach && !isLoading" link to="/register"
+          >Register as Coach</base-button
+        >
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coache-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -26,55 +31,61 @@
 <script>
 import coacheItem from '../../components/coaches/CoachItem.vue';
 import CoachFilter from '../../components/coaches/CoachFilter.vue';
+import BaseSpinner from '../../components/ui/BaseSpinner.vue';
 export default {
-  data(){
+  data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
-        career: true
-      }
-    }
+        career: true,
+      },
+    };
   },
 
   components: {
     coacheItem,
-    CoachFilter
+    CoachFilter,
+    BaseSpinner
   },
   computed: {
-    isCoach(){
-      return this.$store.getters['coachesStore/isCoach']
+    isCoach() {
+      return this.$store.getters['coachesStore/isCoach'];
     },
     filteredCoaches() {
       const coaches = this.$store.getters['coachesStore/coaches'];
-      return coaches.filter((coach)=>{
-        if(this.activeFilters.frontend && coach.areas.includes('frontend')){
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
           return true;
         }
-        if(this.activeFilters.backend && coach.areas.includes('backend')){
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
           return true;
         }
-        if(this.activeFilters.career && coach.areas.includes('career')){
+        if (this.activeFilters.career && coach.areas.includes('career')) {
           return true;
         }
         return false;
-      })
+      });
     },
     hasCoaches() {
       return this.$store.getters['coachesStore/hasCoaches'];
-    }
-  },
-  created(){
-    this.loadCoaches()
-  },
-  methods:{
-    setFilter(filters){
-      this.activeFilters = filters
     },
-    loadCoaches(){
-      this.$store.dispatch('coachesStore/loadCoaches')
-    }
-  }
+  },
+  created() {
+    this.loadCoaches();
+  },
+  methods: {
+    setFilter(filters) {
+      this.activeFilters = filters;
+    },
+    loadCoaches() {
+      this.isLoading = true;
+      this.$store.dispatch('coachesStore/loadCoaches').then(() => {
+        this.isLoading = false;
+      });
+    },
+  },
 };
 </script>
 <style scoped>
